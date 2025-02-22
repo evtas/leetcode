1c5a697d8ee005b522cb66a5d6e04145800eba32
doraeb <13126282133@163.com>
2025-02-22 11:13:29 +0800

    238

--- 238/238.go
+++ 238/238.go
@@ -1,6 +1,6 @@
 package main
 
-func productExceptSelf(nums []int) []int {
+func productExceptSelf1(nums []int) []int {
 	l, r := make([]int, len(nums)), make([]int, len(nums))
 	l[0], l[1] = 1, nums[0]
 	r[len(nums)-1], r[len(nums)-2] = 1, nums[len(nums)-1]
@@ -13,6 +13,25 @@ func productExceptSelf(nums []int) []int {
 	res := make([]int, 0, len(nums))
 	for i := 0; i < len(nums); i++ {
 		res = append(res, l[i]*r[i])
+	}
+	return res
+}
+
+func productExceptSelf(nums []int) []int {
+	n := len(nums)
+	res := make([]int, n)
+	for i := 0; i < n; i++ {
+		res[i] = 1
+	}
+	l, r := 0, n-1
+	lv, rv := 1, 1
+	for r >= 0 && l <= n-1 {
+		res[l] *= lv
+		res[r] *= rv
+		lv *= nums[l]
+		rv *= nums[r]
+		l++
+		r--
 	}
 	return res
 }
